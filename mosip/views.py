ab966f03542958e76d4b08b2d6985f0c9cba56ee
James <71737150+JEGeraldo@users.noreply.github.com>
2026-04-03 16:15:35 +0000

    Update API boilerplates

--- mosip/views.py
+++ mosip/views.py
@@ -2,35 +2,50 @@ from django.http import HttpRequest
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 
 from .models import (
-    MOSIPBaseResponse, MOSIPKYCResponse, MOSIPAuthResponse
+    MOSIPBaseResponse, MOSIPKYCResponse, MOSIPAuthResponse, MOSIPGenOTPResponse
 )
 
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 
 @api_view(['POST'])
 def auth_via_demographics(request : HttpRequest) :
-    pass
+    mosip_response = MOSIPAuthResponse.from_demographics(
+        
+    )
 
 
 @api_view(['POST'])
 def auth_start_otp(request : HttpRequest) :
-    pass
+    mosip_response = MOSIPGenOTPResponse.start_otp(
+
+    )
 
 
 @api_view(['POST'])
 def auth_via_otp(request : HttpRequest) :
-    pass
+    mosip_response = MOSIPAuthResponse.from_otp(
+        
+    )
 
 
 @api_view(['POST'])
 def kyc_via_demographics(request : HttpRequest) :
-    pass
+    mosip_response = MOSIPKYCResponse.from_demographics(
+        
+    )
 
 
 @api_view(['POST'])
 def kyc_start_otp(request : HttpRequest) :
-    pass
+    mosip_response = MOSIPGenOTPResponse.start_otp(
+        
+    )
 
 
 @api_view(['POST'])
 def kyc_via_otp(request : HttpRequest) :
-    pass
+    mosip_response = MOSIPKYCResponse.from_otp(
+        
+    )
