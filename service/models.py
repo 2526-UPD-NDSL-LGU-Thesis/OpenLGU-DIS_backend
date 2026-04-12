f0379c07bc0f0138c6b880b06b7648cbb47d07da
James <jtgeraldo@up.edu.ph>
2026-04-12 11:14:15 +0800

    Implement service dispensing and claiming

--- service/models.py
+++ service/models.py
@@ -1,35 +1,73 @@
-from django.db import models
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User as Official
+from django.db import models, IntegrityError, transaction
 from django.utils import timezone
-from datetime import timedelta
-from residents.models import User
+from residents.models import User as Resident
+
 from .generator import generate_id
 
 
-class Service(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 
-    # Claim rule
-    max_claims = models.IntegerField(null=True, blank=True)
-    period_seconds = models.IntegerField(null=True, blank=True)
+
+class Service(models.Model):
+    # class ClaimPolicy(models.TextChoices):
+    #     PER_USER = "per_user", "Per User"
+    #     SHARED_STOCK = "shared_stock", "Shared Stock"
+
+    # class ClaimTypes(models.TextChoices):
+    #     ONCE = "once", "Once"
+    #     PERIODIC = "periodic", "Periodic"
+    #     COOLDOWN = "cooldown", "Cooldown"
+
+    # class ClaimPeriods(models.TextChoices):
+    #     DAILY = "daily", "Daily"
+    #     WEEKLY = "weekly", "Weekly"
+    #     MONTHLY = "monthly", "Monthly"
+    #     QUARTERLY = "quarterly", "Quarterly"
+    #     YEARLY = "yearly", "Yearly"
+    
+    # class ClaimResetDay(models.TextChoices):
+    #     SUNDAY = "sunday", "Sunday"
+    #     MONDAY = "monday", "Monday"
+    #     TEUSDAY = "teusday", "Teusday"
+    #     WEDNESDAY = "wednesday", "Wednesday"
+    #     THURSDAY = "thursday", "Thursday"
+    #     FRIDAY = "friday", "Friday"
+    #     SATURDAY = "saturday", "Saturday"
+    
+    # class InventoryTypes(models.TextChoices):
+    #     UNLIMITED = "unlimited", "Unlimited"
+    #     LIMITED = "limited", "Limited"
+
+    name = models.CharField(max_length=40, primary_key=True)
+    verbose_name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    # claim_policy = models.CharField(max_length=20, choices=ClaimPolicy, null=True, blank=True)
+    
+    # claim_type = models.CharField(max_length=20, choices=ClaimTypes, null=True, blank=True)
+
+    # claim_period = models.CharField(max_length=20, choices=ClaimPeriods, null=True, blank=True)
+
+    # claim_reset_day = models.CharField(max_length=20, choices=ClaimResetDay, null=True, blank=True)
+
+    stocks = models.PositiveIntegerField()
 
     active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.name
-
-    def remaining_claims(self, user):
-        if not self.max_claims or not self.period_seconds:
-            return None  # unlimited
-
-        period_start = timezone.now() - timedelta(seconds=self.period_seconds)
-
-        count = self.claims.filter(
-            user=user,
-            claimed_at__gte=period_start
-        ).count()
-
-        return max(self.max_claims - count, 0)
+    def __str__(self) -> str :
+        return str(self.name)
+    
+    def save(self, *args, **kwargs) -> None :
+        return super().save(*args, **kwargs)
+    
+    def clean(self) -> None:
+        #TODO: Implement Validations
+        return super().clean()
 
 
 class ServiceClaim(models.Model):
@@ -37,7 +75,7 @@ class ServiceClaim(models.Model):
     transaction_id = models.CharField(unique=True, db_index=True, editable=False)
 
     user = models.ForeignKey(
-        User,
+        Resident,
         on_delete=models.CASCADE,
         related_name="service_claims"
     )
@@ -49,6 +87,8 @@ class ServiceClaim(models.Model):
     )
 
     claimed_at = models.DateTimeField(default=timezone.now)
+
+    claimed_by = models.ForeignKey(Official, on_delete=models.SET_NULL, null=True, related_name="claims_made")
 
     class Meta:
         indexes = [
@@ -70,3 +110,43 @@ class ServiceClaim(models.Model):
                     self.transaction_id = None
             raise ValueError("Failed to Transaction ID")
         return super().save(*args, **kwargs)
+
+    @staticmethod
+    def can_claim(user : Resident, service : Service, amount : int) -> bool :
+        if not service in user.registered_services:
+            return False
+
+        if int(service.stocks) - amount < 0:
+            return False
+
+        return True
+
+
+@transaction.atomic
+def claim(user : Resident, service : Service, amount : int, claimed_by : Official) -> bool :
+    service = (
+        Service.objects
+        .select_for_update()
+        .get(pk=service.pk)
+    )
+
+    if not ServiceClaim.can_claim(user, service, amount):
+        return False
+    
+    service.stocks -= 1
+    service.save(update_fields=["stocks"])
+
+    ServiceClaim.objects.create(
+        user=user,
+        service=service,
+        claimed_by=claimed_by
+    )
+
+    return True
+
+
+class GiveawayService(ServiceClaim):
+    class Meta:
+        proxy = True
+        verbose_name = "Giveaway Claims"
+        verbose_name_plural = "Giveaway Claims"
--- service/views.py
+++ service/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from django.http import HttpRequest, JsonResponse
+from rest_framework import viewsets, status
+from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
-from .models import Service
+from .models import Service, claim
 from .serializers import ServiceSerializer
+from residents.models import User
 
 
 class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,3 +21,45 @@ class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
         active_services = self.queryset.filter(active=True)
         serializer = self.get_serializer(active_services, many=True)
         return Response(serializer.data)
+
+
+@api_view(["POST"])
+def claim_service(request : HttpRequest, service_id : str) -> JsonResponse :
+    data = request.data
+
+    try:
+        service = Service.objects.get(service__name=service_id)
+    except Service.DoesNotExist:
+        return JsonResponse(
+            { "error" : "Service does not exist" },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    user_uin = data["user_uin"]
+    try:
+        resident = User.objects.get(pk=user_uin)
+    except User.DoesNotExist:
+        return JsonResponse(
+            { "error" : "User does not exist" },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    authenticated_user = request.user
+
+    success = claim(
+        user=resident,
+        service=service,
+        amount=1,
+        claimed_by=authenticated_user
+    )
+
+    if not success:
+        return JsonResponse(
+            { "error" : "Failed to claim service" },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    return JsonResponse(
+        { "status" : "Service claimed" },
+        status=status.HTTP_201_CREATED
+    )
