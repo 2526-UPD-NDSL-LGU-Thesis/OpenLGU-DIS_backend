0a736a9daec41716f5c875bee622c252e0fd0d70
James <jtgeraldo@up.edu.ph>
2026-04-21 20:19:39 +0800

    Remove unused functions and views

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,12 @@ Top level router. Sends traffic out to frontend apps.
 
 from django.urls import path, include
 from rest_framework import routers
-from . import views as api_views
 from residents import views as resident_views
 from service import views as service_views
+
+from . import views as api_views
+
+# pylint: disable=trailing-whitespace
 
 router = routers.DefaultRouter()
 router.register(r'ids', resident_views.UserViewSet)
@@ -17,14 +20,6 @@ urlpatterns = [
     path('', include('mosip.urls')),
     path('', include('qr_manager.urls')),
     path('', include('service.urls')),
-    # path('read/', api_views.read, name='read'),
-    # path('verify/', api_views.verify, name='verify'),
-#    path('startotp/', api_views.start_verify_otp, name='startotp'),
-#    path('otp/', api_views.verify_otp, name='otp'),
-    # path('digitalid/', api_views.digitalid, name='digitalid'),
-    # path('authenticate/', api_views.authenticate_message, name='authenticate'),
-    # path('claim/', api_views.claim, name='claim'),
-    # path('upload-qr/', api_views.upload_qr, name='upload-qr'),
     path('login/', api_views.login_view),
     path('ping/', api_views.ping),
     path('user/ping/', api_views.user_ping),
