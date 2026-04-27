092140d6536fb2ef39ea9a2a26a6a715df8191ed
James <jtgeraldo@up.edu.ph>
2026-04-27 13:33:47 +0800

    Update urls.py

--- service/urls.py
+++ service/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('service/<str:service_id>/', views.claim_service),
-    path('service/<str:service_id>/pcn/', views.claim_service_with_pcn)
+    path('claim/<str:service_id>/', views.claim_service),
+    path('claim/<str:service_id>/pcn/', views.claim_service_with_pcn)
 ]
 #TODO: Define a better URL scheme
