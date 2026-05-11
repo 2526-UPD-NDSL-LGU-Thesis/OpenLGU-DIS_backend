c0f170aaba2d5b962dc27113a7fb6844ff4fc1fc
James <jtgeraldo@up.edu.ph>
2026-05-11 12:50:00 +0800

    Revert changes

--- app/settings.py
+++ app/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-        "rest_framework.permissions.DjangoModelPermissions",
+        # "rest_framework.permissions.DjangoModelPermissions",
     ],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.UserRateThrottle",
