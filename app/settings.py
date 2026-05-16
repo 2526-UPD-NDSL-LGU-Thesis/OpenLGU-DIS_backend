e5b4a1c47e8c2f76903b924ac524fb8b016e5852
James <jtgeraldo@up.edu.ph>
2026-05-16 20:31:16 +0800

    Set Default Permission to Django Model Permissions

--- app/settings.py
+++ app/settings.py
@@ -51,8 +51,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-        # "rest_framework.permissions.DjangoModelPermissions",
+        # 'rest_framework.permissions.IsAuthenticated',
+        "rest_framework.permissions.DjangoModelPermissions",
     ],
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.UserRateThrottle",
