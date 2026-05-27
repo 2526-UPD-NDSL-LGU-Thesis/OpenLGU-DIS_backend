adfda1ec68b23d1a7e4cacbb298d781ad4770318
James <jtgeraldo@up.edu.ph>
2026-05-27 11:12:52 +0800

    Redirect root to admin

--- app/urls.py
+++ app/urls.py
@@ -18,11 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='admin/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('residents.urls')),
     # path('', include('organization.urls')),
     # path('', include('template_builder.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
