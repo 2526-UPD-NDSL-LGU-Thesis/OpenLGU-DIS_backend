94b18002ac5ef551c258f646225ac253b3fb9196
James <jtgeraldo@up.edu.ph>
2026-05-28 20:49:27 +0800

    Revert face image to JPEG format

--- qr_manager/utils.py
+++ qr_manager/utils.py
@@ -387,7 +387,8 @@ def generate_qr(**kwargs) -> bytes :
     image = qr.make_image(fill_color="black", back_color="white")
 
     buffer = BytesIO()
-    image.save(buffer, format="WEBP")
+    # image.save(buffer, format="WEBP")
+    image.save(buffer, format="JPEG")
     image_bytes = buffer.getvalue()
 
     return image_bytes
